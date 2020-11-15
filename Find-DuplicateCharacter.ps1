db83d1ffdf474cd0aafc093299b95af805a7171c
Joel Bennett <Jaykul@HuddledMasses.org>
2020-11-15 12:05:32 -0500

    Add examples, pipeline input. Switch back to index
    After testing, they work equally fast
    And this will use less memory

--- Find-DuplicateCharacter.ps1
+++ Find-DuplicateCharacter.ps1
@@ -6,18 +6,32 @@ function Find-DuplicateCharacter {
             Find-DuplicateCharacter 'abcdedcba'
 
             # Returns the first duplicate character, 'd'
+        .EXAMPLE
+            $UnicodeString = $ExecutionContext.InvokeCommand.ExpandString(-join @(19968..40908).ForEach{"``u{$("{0:x}" -f $_)}"})
+            $UnicodeString += $UnicodeString.ToCharArray() | Get-Random -OutVariable char
+            $Result = Find-DuplicateCharacter $UnicodeString
+            $Char -eq $Result
+
+            # This example shows one way to test the function:
+            # - How to generate a really long string of unique unicode characters
+            # - How to guarantee it has a duplicate by appending a copy of a random character
+            # - How to use Find-DuplicateCharacter to find the duplicate character
+            # - How to verify the duplicate was the right character
     #>
     [CmdletBinding()]
     param(
         # An arbitrarily long string
         [Parameter(Mandatory, ValueFromPipeline)]
+        [Alias("InputObject")]
         [string]$foo
     )
-    $unique = [System.Collections.Generic.HashSet[char]]::new()
-    foreach ($char in $foo.ToCharArray()) {
-        if (-not $unique.Add($char)) {
-            $char
-            return
+    process {
+        $unique = [System.Collections.Generic.HashSet[char]]::new()
+        for ($i = 0; $i -lt $foo.Length; $i++) {
+            if (-not $unique.Add($foo[$i])) {
+                $foo[$i]
+                return
+            }
         }
     }
 }
